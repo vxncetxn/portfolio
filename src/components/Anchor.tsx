4d7b92582de35d5cb9204ad2516faef5b471b787
vxncetxn <thevancetan@gmail.com>
2022-10-07 21:00:20 +0800

    Added base for link previews in Anchor

--- src/components/Anchor.tsx
+++ src/components/Anchor.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface AnchorProps {
   href: string;
   font?: "sans" | "serif";
@@ -11,16 +13,27 @@ export function Anchor({
   size = "base",
   children,
 }: AnchorProps) {
+  const [previewOpen, setPreviewOpen] = useState(false);
+
   return (
-    <a
-      href={href}
-      target="_blank"
-      rel="noopener"
-      className={`${font === "sans" ? "font-sans" : "font-serif"} ${
-        size === "small" ? "text-18" : "base" ? "text-24" : "text-40"
-      } text-neutral-02 underline transition-colors selection:bg-theme-selected rounded-2 focus-visible:outline-none focus-visible:ring focus-visible:ring-theme-selected focus-visible:ring-offset-2 focus-visible:ring-offset-transparent`}
+    <span
+      onMouseEnter={() => setPreviewOpen(true)}
+      onMouseLeave={() => setPreviewOpen(false)}
+      className="relative"
     >
-      {children}
-    </a>
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener"
+        className={`${font === "sans" ? "font-sans" : "font-serif"} ${
+          size === "small" ? "text-18" : "base" ? "text-24" : "text-40"
+        } text-neutral-02 underline transition-colors selection:bg-theme-selected rounded-2 focus-visible:outline-none focus-visible:ring focus-visible:ring-theme-selected focus-visible:ring-offset-2 focus-visible:ring-offset-transparent`}
+      >
+        {children}
+      </a>
+      {previewOpen ? (
+        <div className="absolute left-1/2 -top-200 -translate-x-1/2 -translate-y-16 bg-white h-200 aspect-video rounded-8 shadow-2xl"></div>
+      ) : null}
+    </span>
   );
 }
